93e59a1c535998ec1a7ea06963c83e4b657b97da
jay <jayleungg@gmail.com>
2022-07-21 12:21:04 -0400

    react first push

---
+++ site/src/components/Park.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Search from '/components/Search'
+
+const Park = () => {
+return(
+    <div>
+        <h1> Coaster Crazy </h1>
+            <Search/>
+
+</div>
+)
+}
+export default Park
