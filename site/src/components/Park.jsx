eebc6eb25014810bbbc48d1c28f099cd727e3e4c
jay <jayleungg@gmail.com>
2022-07-21 16:32:46 -0400

    parks page

--- site/src/components/Park.jsx
+++ site/src/components/Park.jsx
@@ -3,9 +3,16 @@ import Search from './Search';
 
 const Park = () => {
 	return (
-		<div>
-			<h1 className='coasterTitle'> Coaster Crazy </h1>
-			<Search />
+
+		<div className = 'parkPage'>
+			
+			<h1>Park Name </h1>
+			<img src='https://media.gettyimages.com/photos/in-this-aerial-view-from-a-drone-rides-sit-idle-at-six-flags-kingdom-picture-id1219052377?s=612x612'></img>
+			<h3>City, State</h3>
+			<p>Coaster Count</p>
+
+
+			
 		</div>
 	);
 };
