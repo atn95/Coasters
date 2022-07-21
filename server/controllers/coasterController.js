98bd86d3526f9dff2a39f11e76f3f14ed2de9971
atn95 <antannguyen2000@gmail.com>
2022-07-21 13:51:13 -0500

    fixed long searches

--- server/controllers/coasterController.js
+++ server/controllers/coasterController.js
@@ -5,23 +5,27 @@ const getAllCoasters = async (req, res) => {
 		const coasters = await Coaster.find({});
 		if (coasters.length > 0) {
 			res.json(coasters);
+		} else {
+			res.send('no coasters found');
 		}
-		res.send('no coasters found');
 	} catch (error) {
 		console.log(error);
 	}
 };
 
 const getCoasterByID = async (req, res) => {
-	let { id } = req;
+	let { id } = req.params;
 	try {
-		let park = await CoasterPlant.findById(id);
+		let park = await Coaster.findById(id);
+		console.log(park);
 		if (park) {
 			res.json(park[0]);
 		} else {
 			res.send('invalid coaster');
 		}
-	} catch (error) {}
+	} catch (error) {
+		console.log(error);
+	}
 };
 
 module.exports = {
