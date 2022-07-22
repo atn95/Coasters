5b6e79f4c10959015723cc530fa465779cd8921c
atn95 <antannguyen2000@gmail.com>
2022-07-22 10:54:20 -0500

    coasters seed

--- site/src/components/Home.jsx
+++ site/src/components/Home.jsx
@@ -3,6 +3,7 @@ import Search from './Search';
 import axios from 'axios';
 import { useState, useEffect } from 'react';
 import ParkCard from './ParkCard';
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
 	let [parks, setParks] = useState([]);
@@ -10,17 +11,18 @@ const Home = () => {
 	useEffect(() => {
 		async function getParks() {
 			let parks = axios.get('http://localhost:3001/parks').then((res) => {
-				console.log(res.data);
 				setParks(res.data);
 			});
 		}
 		getParks();
 	}, []);
 
+	let navigate = useNavigate();
+
 	useEffect(() => {
 		const getParks = async () => {
 			const response = await fetch(
-				'https://docs.google.com/spreadsheets/d/1gSsbtsN-lxL98gCOhs9q80APHs69lhwquXSJ5vRUQGg/gviz/tq?sheet=Parks'
+				'https://docs.google.com/spreadsheets/d/1gSsbtsN-lxL98gCOhs9q80APHs69lhwquXSJ5vRUQGg/gviz/tq?sheet=Coasters'
 			);
 			let data = await response.text();
 			let parsedResponse = JSON.parse(data.substring(47).slice(0, -2)).table
@@ -28,14 +30,19 @@ const Home = () => {
 			let parksArray = [];
 			parsedResponse.forEach((row) => {
 				let rowInfo = [];
+				console.log(row);
 				row.c.forEach((col) => {
-					rowInfo.push(col.v);
+					// console.log()
+					if (col.v != null) rowInfo.push(col.v);
 				});
 				let obj = {
-					park_name: rowInfo[0],
-					city: rowInfo[1],
-					state: rowInfo[2],
-					image: rowInfo[3],
+					name: rowInfo[0],
+					desccription: rowInfo[1],
+					height: rowInfo[2],
+					top_speed: rowInfo[3],
+					duration: rowInfo[4],
+					image: rowInfo[5],
+					park_id: rowInfo[6],
 				};
 				parksArray.push(obj);
 			});
@@ -47,12 +54,6 @@ const Home = () => {
 	const itemsPerRow = 3;
 
 	const styles = {
-		flexContainer: {
-			display: 'flex',
-			flexDirection: 'row',
-			flexWrap: 'wrap',
-			border: '2px solid red',
-		},
 		gridContainer: {
 			display: 'grid',
 			gridTemplateColumns: `repeat(${itemsPerRow}, 1fr)`,
@@ -68,7 +69,11 @@ const Home = () => {
 			</div>
 			<div className='parks' style={styles.gridContainer}>
 				{parks.map((park) => (
-					<ParkCard name={park.park_name} image={park.image} />
+					<ParkCard
+						name={park.park_name}
+						image={park.image}
+						onClick={() => navigate(`park/${park._id}`)}
+					/>
 				))}
 			</div>
 		</div>
